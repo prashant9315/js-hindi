856d0934408876282069e8966fb8dcd337233b4f
prashant9315 <137028444+prashant9315@users.noreply.github.com>
2023-10-18 00:41:09 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we are using node js ,not browser
+
+let name = "hitesh";
+let age = 18;
+let isLogggedIn = true;
+
+//different datatypes
+//number => 2 to power 53 is limit
+//bigInt =>> for geater than number
+//string = ""
+//boolean= true/false
+//null=standalone value,its datatype is object
+//undefined = that as not given any value,its datatype is undefined
+//symbol =>for unique things
+
+//object
+
+console.log(typeof age); //typeof => to give about datatypes of varibles
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
