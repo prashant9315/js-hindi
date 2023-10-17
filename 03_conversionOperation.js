8dd05b1dbf45edda873c221ae5ceb3bab8541646
prashant9315 <137028444+prashant9315@users.noreply.github.com>
2023-10-18 01:00:30 +0530

    convertionInDatatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33";
+
+let valueInNumber = Number(score); //converting in number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NaN WILL BE output in console when convert in number
+//true=1,false=0
+
+let isLoggedIn = "hitesh";
+
+let booleanIn = Boolean(isLoggedIn); //coverting in boolean
+
+//1 =>true; 0=>false;
+//"" =>false,if empty string then false
+//"hitesh"=>true
+
+let someNumber = 28;
+
+let InString = String(someNumber); //coverting in string
