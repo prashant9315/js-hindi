d55f7a66440adc9cfb7f330a18a4d8a716681c29
prashant9315 <137028444+prashant9315@users.noreply.github.com>
2024-01-21 00:16:41 +0530

    15_DOM

---
+++ 15_DOM/one.js
@@ -0,0 +1,22 @@
+//document.getElementById('title')->it will give tittle of html page
+
+//document.getElementById('title').getAttribute->it will give attributes of id
+
+//document.getElementById('title').setAttribute()->it will change attribute
+
+//------------------------------------------------------------------------->
+
+//innertext,innerHTML,textcontent->they all give text node of given function
+//(see on html page)
+//title.textcontent->it will give only upper text node like "DOM learning on Chai aur code"
+
+//title.innerText->it will give all the text node like "DOM learning on Chai aur code test text"
+
+//title.innerHTML->it will give all text node with their html tag also like " DOM learning on Chai aur code<span>test text</span>"
+
+//--------------------------------------------------------------------------->
+
+//querySelector->it is same as getelementbyid ,as it will select one element
+//querySelctorAll->to select all element
+
+//Array.from()->any value to convert in array
